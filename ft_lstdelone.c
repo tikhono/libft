aa015ac58553bf703592cbcb5fff0a1aa8dd1a6f
Andriy TIKHONOV <atikhono@e1r9p7.unit.ua>
2018-01-20 16:04:53 +0200

    clean lib from vogsphere

---
+++ ft_lstdelone.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atikhono <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/16 17:10:21 by atikhono          #+#    #+#             */
+/*   Updated: 2017/11/18 18:46:36 by atikhono         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+{
+	if (alst != NULL && *alst != NULL && del != NULL)
+	{
+		del((*alst)->content, (*alst)->content_size);
+		free(*alst);
+		*alst = NULL;
+	}
+}

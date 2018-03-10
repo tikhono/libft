2661fe4e6cb010f867851cdd01fc4e4740a503d3
atikhono <tihonovandrey11@gmail.com>
2018-03-10 13:43:04 +0200

    add gnl, remove shitty lists

--- libft/libft.h
+++ libft/libft.h
@@ -6,32 +6,22 @@
 /*   By: atikhono <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/15 19:20:12 by atikhono          #+#    #+#             */
-/*   Updated: 2018/03/10 13:39:30 by atikhono         ###   ########.fr       */
+/*   Updated: 2017/11/16 15:40:33 by atikhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-# define BUFF_SIZE 1024
 # include <unistd.h>
 # include <string.h>
 # include <stdlib.h>
-# include <sys/types.h>
-# include <sys/uio.h>
 
-typedef struct		s_llist
+typedef struct		s_list
 {
-	int				fd;
-	char			*buff;
-	struct s_llist	*next;
-}					t_llist;
-
-typedef struct		s_operator
-{
-	char			*to_free;
-	char			*temp;
-	char			*n;
-}					t_operator;
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
 
 void				*ft_memset(void *b, int c, size_t len);
 void				ft_bzero(void *s, size_t n);
@@ -88,12 +78,17 @@ void				ft_putstr_fd(char const *s, int fd);
 void				ft_putendl_fd(char const *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
 
+t_list				*ft_lstnew(void const *content, size_t content_size);
+void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstadd(t_list **alst, t_list *new);
+void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+
 char				*ft_itoa_base(int num, int base);
 void				ft_reverse(char *str);
 int					ft_find_digit(int a, int base);
 long long int		ft_power(long long int a, int n);
 int					ft_sqrt(int a, int n);
-
-int					get_next_line(const int fd, char **line);
 
 #endif

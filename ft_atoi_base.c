ec1a44d0360ab502cdd0f145d9d0b1174f74ac66
atikhono <tihonovandrey11@gmail.com>
2018-05-11 16:40:30 +0300

    norme

--- ft_atoi_base.c
+++ ft_atoi_base.c
@@ -6,32 +6,32 @@
 /*   By: atikhono <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/03 20:26:26 by atikhono          #+#    #+#             */
-/*   Updated: 2018/04/08 19:11:54 by atikhono         ###   ########.fr       */
+/*   Updated: 2018/05/11 16:39:02 by atikhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_atoi_base(const char *str, int base)
+int		ft_atoi_base(const char *s, int base)
 {
 	size_t		i;
 	size_t		res;
 	int			symbol;
 
 	i = 0;
-	while ((str[i] >= 0 && str[i] <= 20) || str[i] == ' ')
+	while ((s[i] >= 0 && s[i] <= 20) || s[i] == ' ')
 		++i;
 	res = 0;
 	symbol = 1;
-	if (str[i] == '-')
+	if (s[i] == '-')
 		symbol = -1;
-	if (str[i] == '-' || str[i] == '+')
+	if (s[i] == '-' || s[i] == '+')
 		++i;
 	if (base == 16)
 		i += 2;
-	while ((str[i] >= '0' && str[i] <= '9') || (str[i] >= 'A' && str[i] <= 'F'))
+	while ((s[i] >= '0' && s[i] <= '9') || (s[i] >= 'A' && s[i] <= 'F'))
 	{
-		res = res * base + ((str[i] >= 'A') ? str[i++] - 'A' + 10 : str[i++] - '0');
+		res = res * base + ((s[i] >= 'A') ? s[i++] - 'A' + 10 : s[i++] - '0');
 		if (res > 9223372036854775807U && symbol == 1)
 			return (-1);
 		if (res > 9223372036854775808U && symbol == -1)
